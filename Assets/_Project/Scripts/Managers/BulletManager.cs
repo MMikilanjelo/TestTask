65467d47e3c8d67cc1e73998535ca0cad11212e8
MMikilanjelo <144453227+MMikilanjelo@users.noreply.github.com>
2024-07-25 17:33:39 +0300

    (feat) Clean Up

--- Assets/_Project/Scripts/Managers/BulletManager.cs
+++ Assets/_Project/Scripts/Managers/BulletManager.cs
@@ -3,6 +3,12 @@ using Game.Core;
 namespace Game.Managers {
 	public class BulletManager : Singleton<BulletManager> {
 		private Bullet selectedBulletPrefab_ = null;
+		protected void Start() {
+			if (ResourceSystem.Instance.TryGetBulletData(BulletTypes.REGULAR, out BulletData data)) {
+				selectedBulletPrefab_ = data.Prefab;
+			}
+			base.Awake();
+		}
 		public Bullet Get() => selectedBulletPrefab_;
 		public void SetSelectedBullet(Bullet bullet) => selectedBulletPrefab_ = bullet;
 
